b891dbaf99b5a183c5bc9125fbb7e6d221d899b9
LeomharT <leo.liao.lzy@gmail.com>
2021-08-28 10:57:20 +0800

    地图搜索

--- src/components/Home/MapSearch.tsx
+++ src/components/Home/MapSearch.tsx
@@ -10,7 +10,10 @@ const MapSearchWrapper = styled.div`
 position: relative;
 `;
 const { Search } = Input;
-
+const shapOfGuLou = '26.101265,119.240802;26.105967,119.243465;26.111208,119.249473;26.112364,119.254623;26.112846,119.257951;26.112595,119.256557;26.113327,119.259006;26.113481,119.258963;26.113443,119.259135;26.114599,119.260722;26.115292,119.261495;26.116448,119.261924;26.117373,119.262954;26.122459,119.274809;26.122459,119.283736;26.121535,119.299175;26.120148,119.303123;26.073437,119.311019;26.072974,119.303638;26.075595,119.303638;26.072512,119.283897;26.070661,119.284412;26.056474,119.271022';
+const shapeOfCangShan = '26.100880,119.225712;26.104733,119.234467;26.104579,119.238758;26.099646,119.242020;26.094097,119.245110;26.082842,119.250603;26.074362,119.255753;26.048301,119.291287;26.046450,119.292317;26.044599,119.306050;26.046142,119.310169;26.049226,119.318066;26.050768,119.326649;26.047684,119.346047;26.045216,119.352741;26.041515,119.360981;26.032723,119.370766;26.019612,119.384670;26.010355,119.402695;26.002333,119.428101;25.994464,119.437199;25.989218,119.440289;25.970700,119.432736;25.964064,119.427071;25.958971,119.395142;25.957890,119.386730;25.963292,119.379349;25.967614,119.372483;25.971009,119.369908;25.973015,119.359951;25.974558,119.345360;25.977490,119.325276;25.978108,119.315148;25.982120,119.286480;25.990452,119.269657;25.996778,119.262963;26.013904,119.248371;26.025011,119.240647;26.044754,119.233780;26.082380,119.222622;26.097488,119.222965';
+const shapeOfTaiJiang = '26.056937,119.271537;26.064956,119.281150;26.069813,119.291874;26.072049,119.291101;26.072820,119.319774;26.069659,119.321053;26.069775,119.322813;26.060253,119.323714;26.060253,119.348179;26.057785,119.360023;26.053082,119.364744;26.042286,119.367662;26.037890,119.367233;26.049535,119.338313;26.049535,119.315482;26.042903,119.302264';
+const shapeOfJinAn = '26.110437,119.317283;26.086851,119.320201;26.066652,119.335479;26.056783,119.364661;26.056783,119.367751;26.062334,119.370498;26.067886,119.372558;26.082688,119.370498;26.097797,119.370841;26.120302,119.376678;26.135097,119.367408;26.137255,119.358825;26.137871,119.347838;26.138179,119.337882;26.138179,119.330329;26.135714,119.324492;26.130782,119.317283';
 @observer
 class MapSearch extends Component<RouteComponentProps, {}>
 {
@@ -18,6 +21,7 @@ class MapSearch extends Component<RouteComponentProps, {}>
     InitMapArea = () =>
     {
         const { history } = this.props;
+        const { DrawShapOfRegion } = this;
         /**
          * 初始化地图
          */
@@ -26,7 +30,7 @@ class MapSearch extends Component<RouteComponentProps, {}>
             zoom: 12.5,
             viewMode: "2D"
         });
-        const circleStyles = new TMap.CircleStyle({
+        const polyonStyles = new TMap.PolygonStyle({
             'color': 'rgba(41,91,255,0.16)',
             'showBorder': true,
             'borderColor': 'rgba(41,91,255,1)',
@@ -39,17 +43,6 @@ class MapSearch extends Component<RouteComponentProps, {}>
             'angle': 0,
             'alignment': 'center',
             'verticalAlignment': 'middle'
-        });
-        new TMap.MultiCircle({
-            map,
-            styles: {
-                'circle': circleStyles
-            },
-            geometries: [{
-                styleId: 'circle',
-                center: new TMap.LatLng(26.099148, 119.271446),
-                radius: 850,
-            }],
         });
         const labelGuLou = new TMap.MultiLabel({
             id: 'labelGuLou',
@@ -65,41 +58,18 @@ class MapSearch extends Component<RouteComponentProps, {}>
                 'content': '鼓楼区',
             }]
         });
-        new TMap.MultiCircle({
-            map,
-            styles: {
-                'circle': circleStyles,
-            },
-            geometries: [{
-                styleId: 'circle',
-                center: new TMap.LatLng(26.046922, 119.274346),
-                radius: 850,
-            }],
-        });
         const labelCangShang = new TMap.MultiLabel({
             id: 'labelCangShang',
             map,
             styles: {
                 'label': labelStyles
             },
-
             geometries: [{
                 'id': 'label_1',
                 'styleId': 'label',
-                'position': new TMap.LatLng(26.046922, 119.274346),
+                'position': new TMap.LatLng(26.012901, 119.293605),
                 'content': '仓山区',
             }]
-        });
-        new TMap.MultiCircle({
-            map,
-            styles: {
-                'circle': circleStyles,
-            },
-            geometries: [{
-                styleId: 'circle',
-                center: new TMap.LatLng(26.053346, 119.333938),
-                radius: 850,
-            }],
         });
         const labelTaiJiang = new TMap.MultiLabel({
             id: 'labelTaiJiang',
@@ -110,9 +80,31 @@ class MapSearch extends Component<RouteComponentProps, {}>
             geometries: [{
                 'id': 'label_1',
                 'styleId': 'label',
-                'position': new TMap.LatLng(26.053346, 119.333938),
+                'position': new TMap.LatLng(26.053496, 119.332036),
                 'content': '台江区',
             }]
+        });
+        const labelJinAn = new TMap.MultiLabel({
+            id: 'labelJinAn',
+            map,
+            styles: {
+                'label': labelStyles
+            },
+            geometries: [{
+                'id': 'label_1',
+                'styleId': 'label',
+                'position': new TMap.LatLng(26.102418, 119.345718),
+                'content': '晋安区',
+            }]
+        });
+        const polygon = new TMap.MultiPolygon({
+            id: 'polygon-layer',
+            map: map,
+            //多边形样式
+            styles: {
+                'polygon': polyonStyles
+            },
+            geometries: []
         });
         labelGuLou.on("click", () =>
         {
@@ -126,6 +118,114 @@ class MapSearch extends Component<RouteComponentProps, {}>
         {
             history.push("HouseList/Exhibits/台江区");
         });
+        labelJinAn.on('click', () =>
+        {
+            history.push("HouseList/Exhibits/晋安区");
+        });
+        //@ts-ignore
+        const marker = new SvgMarker({
+            map,
+            options: [
+                {
+                    position: new TMap.LatLng(26.099873, 119.271895),
+                    id: 0,
+                },
+                {
+                    position: new TMap.LatLng(26.012901, 119.293605),
+                    id: 1
+                },
+                {
+                    position: new TMap.LatLng(26.053496, 119.332036),
+                    id: 2,
+                },
+                {
+                    position: new TMap.LatLng(26.102418, 119.345718),
+                    id: 3,
+                }
+            ]
+        });
+        marker.on('mouseenter', function (e: any, region: any)
+        {
+            switch (region)
+            {
+                case "鼓楼区": {
+                    polygon.add([{
+                        'id': 'shapeGulou',
+                        'styleId': 'polygon',
+                        'paths': DrawShapOfRegion(shapOfGuLou)
+                    }]);
+                    break;
+                }
+                case "仓山区": {
+                    polygon.add([{
+                        'id': 'shapeCangShang',
+                        'styleId': 'polygon',
+                        'paths': DrawShapOfRegion(shapeOfCangShan)
+                    }]);
+                    break;
+                }
+                case '台江区': {
+                    polygon.add([{
+                        'id': 'shapeTaiJiang',
+                        'styleId': 'polygon',
+                        'paths': DrawShapOfRegion(shapeOfTaiJiang)
+                    }]);
+                    break;
+                }
+                case '晋安区': {
+                    polygon.add([{
+                        'id': 'shapeJinAn',
+                        'styleId': 'polygon',
+                        'paths': DrawShapOfRegion(shapeOfJinAn)
+                    }]);
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+            e.setAttribute('style', 'fill: red;stroke:red; opacity:0.9;');
+        });
+        marker.on('mouseleave', function (e: any, region: any)
+        {
+            switch (region)
+            {
+                case "鼓楼区": {
+                    polygon.remove(['shapeGulou']);
+                    break;
+                }
+                case "仓山区": {
+                    polygon.remove(['shapeCangShang']);
+                    break;
+                }
+                case '台江区': {
+                    polygon.remove(['shapeTaiJiang']);
+                    break;
+                }
+                case '晋安区': {
+                    polygon.remove(['shapeJinAn']);
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+            e.setAttribute('style', 'fill: blue;stroke:#FFFFFF;opacity:0.9;');
+        });
+
+    };
+    //@ts-ignore
+    DrawShapOfRegion = (polygon: string): Array<TMap.LatLng> =>
+    {
+        //@ts-ignore
+        const shapeOfRegionArray: TMap.LatLng[] = [];
+        for (let p of polygon.split(";"))
+        {
+            shapeOfRegionArray.push(
+                new TMap.LatLng(parseFloat(p.split(",")[0]), parseFloat(p.split(",")[1]))
+            );
+        }
+        return shapeOfRegionArray;
     };
     componentDidMount = () =>
     {
