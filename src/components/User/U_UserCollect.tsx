54799281943af37ce3c5c6f8766b66f4ca56881e
LeomharT <leo.liao.lzy@gmail.com>
2021-09-10 08:35:36 +0800

    用户添加收藏显示

--- src/components/User/U_UserCollect.tsx
+++ src/components/User/U_UserCollect.tsx
@@ -1,12 +1,38 @@
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
 import React, { Component } from 'react';
+import { HouseBaseInfo } from '../../interfaces/HouseListInterface';
+import UserStore from '../../redux/UserStore';
+import { CONST_HOST } from '../Common/VariableGlobal';
+import HouseItem from '../HouseList/HouseItem';
 
+@observer
 export default class U_UserCollect extends Component
 {
+    UserStore: UserStore = UserStore.GetInstance();
+    @observable userCollections: HouseBaseInfo[] = [];
+    InitUserCollections = async () =>
+    {
+        const { id } = this.UserStore.authInfo?.userInfo;
+        this.userCollections = (await (await fetch(`${CONST_HOST}/GetAllUserCollections?id=${id}`)).json()) as HouseBaseInfo[];
+    };
+    async componentDidMount()
+    {
+        await this.InitUserCollections();
+    }
     render()
     {
+        const { userCollections } = this;
         return (
-            <div>
-                我是用户收藏
+            <div className='U_Collections'>
+                {
+                    userCollections.map((h: HouseBaseInfo) =>
+                    {
+                        return (
+                            <HouseItem key={h.hId} HouseInfo={h} />
+                        );
+                    })
+                }
             </div>
         );
     }
