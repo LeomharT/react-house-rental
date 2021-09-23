2117cd396c8a3d527c03548c01f50c050c0dbbb7
LeomharT <leo.liao.lzy@gmail.com>
2021-09-24 07:37:10 +0800

    评论UI

--- src/components/HouseList/HComment.tsx
+++ src/components/HouseList/HComment.tsx
@@ -9,13 +9,74 @@ import EmojiList from '../HConsult/EmojiList';
 @observer
 export default class HComment extends Component<{}, {}>
 {
+
+    render()
+    {
+        return (
+            <div className='HComment' id='HComment'>
+                <CommentInput />
+                <Divider />
+                <CommentItem children={
+                    <CommentItem />
+                } />
+            </div>
+        );
+    }
+}
+@observer
+export class CommentInput extends React.Component<{}, {}>
+{
     UserStore: UserStore = UserStore.GetInstance();
     messageTextArea: RefObject<HTMLTextAreaElement> = React.createRef<HTMLTextAreaElement>();
     fileUploader: RefObject<HTMLInputElement> = React.createRef<HTMLInputElement>();
     render()
     {
         return (
-            <div className='HComment' id='HComment'>
+            <div className='CommentInput'>
+                <Avatar
+                    size={46}
+                    shape='circle'
+                    src={
+                        this.UserStore.authInfo.userInfo
+                            ? this.UserStore.authInfo.userInfo.photo
+                            : 'https://files.authing.co/authing-console/default-user-avatar.png'
+                    }
+                />
+                <div>
+                    <textarea ref={this.messageTextArea} rows={4} />
+                    <div>
+                        <Popover
+                            trigger='click'
+                            placement='bottomLeft'
+                            content={
+                                <EmojiList messageInput={this.messageTextArea} />
+                            }
+                        >
+                            <Button
+                                type='link'
+                                icon={<SmileOutlined />}
+                            />
+                        </Popover>
+                        <Button
+                            type='link'
+                            icon={<PictureOutlined />}
+                            onClick={() =>
+                            {
+                                this.fileUploader.current!.click();
+                            }}
+                        />
+                    </div>
+                    <Button
+                        type='primary'
+                        icon={<CommentOutlined />}
+                        style={{ width: "200px" }}
+                        onClick={() =>
+                        {
+                            this.UserStore.CheckForIsLogin();
+                        }}
+                    >发布评论
+                    </Button>
+                </div>
                 <input
                     type='file'
                     style={{ display: "none" }}
@@ -24,58 +85,11 @@ export default class HComment extends Component<{}, {}>
                     {
 
                     }} />
-                <div className='CommentInput'>
-                    <Avatar
-                        size={46}
-                        shape='circle'
-                        src={
-                            this.UserStore.authInfo.userInfo
-                                ? this.UserStore.authInfo.userInfo.photo
-                                : 'https://files.authing.co/authing-console/default-user-avatar.png'
-                        }
-                    />
-                    <div>
-                        <textarea ref={this.messageTextArea} rows={4} />
-                        <div>
-                            <Popover
-                                trigger='click'
-                                content={
-                                    <EmojiList messageInput={this.messageTextArea} />
-                                }
-                            >
-                                <Button
-                                    type='link'
-                                    icon={<SmileOutlined />}
-                                />
-                            </Popover>
-                            <Button
-                                type='link'
-                                icon={<PictureOutlined />}
-                                onClick={() =>
-                                {
-                                    this.fileUploader.current!.click();
-                                }}
-                            />
-                        </div>
-                        <Button
-                            type='primary'
-                            icon={<CommentOutlined />}
-                            style={{ width: "200px" }}
-                            onClick={() =>
-                            {
-                                this.UserStore.CheckForIsLogin();
-                            }}
-                        >
-                            发布评论
-                        </Button>
-                    </div>
-                </div>
-                <Divider />
-                <CommentItem />
             </div>
         );
     }
 }
+
 @observer
 export class CommentItem extends React.Component<{}, {}>
 {
@@ -83,8 +97,27 @@ export class CommentItem extends React.Component<{}, {}>
     @observable likes: number = 0;
     @observable dislikes: number = 0;
     @observable liked: string = "";
+    @observable showReplay: boolean = false;
     render()
     {
+        //汉化时间
+        moment.defineLocale('zh-cn', {
+            relativeTime: {
+                future: '%s内',
+                past: '%s前',
+                s: '几秒',
+                m: '1 分钟',
+                mm: '%d 分钟',
+                h: '1 小时',
+                hh: '%d 小时',
+                d: '1 天',
+                dd: '%d 天',
+                M: '1 个月',
+                MM: '%d 个月',
+                y: '1 年',
+                yy: '%d 年'
+            },
+        });
         const actions = [
             <Tooltip title='赞'>
                 <span onClick={() =>
@@ -108,36 +141,46 @@ export class CommentItem extends React.Component<{}, {}>
                     <span style={{ paddingLeft: "5px" }}>{this.dislikes}</span>
                 </span>
             </Tooltip>,
-            <span>回复</span>,
+            <span
+                onClick={() =>
+                {
+                    this.showReplay = !this.showReplay;
+                }}
+            >回复
+            </span>,
         ];
         return (
-            <Comment
-                actions={actions}
-                author={<span>Han Solo</span>}
-                avatar={
-                    <Avatar
-                        size={46}
-                        shape='circle'
-                        src={
-                            this.UserStore.authInfo.userInfo
-                                ? this.UserStore.authInfo.userInfo.photo
-                                : 'https://files.authing.co/authing-console/default-user-avatar.png'
-                        }
-                    />
-                }
-                content={
-                    <p>
-                        We supply a series of design principles, practical patterns and high quality design
-                        resources (Sketch and Axure), to help people create their product prototypes beautifully
-                        and efficiently.
-                    </p>
-                }
-                datetime={
-                    <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
-                        <span>{moment().fromNow()}</span>
-                    </Tooltip>
-                }
-            />
+            <div className='CommentItem'>
+                <Comment
+                    actions={actions}
+                    author={<span>Han Solo</span>}
+                    avatar={
+                        <Avatar
+                            size={46}
+                            shape='circle'
+                            src={
+                                this.UserStore.authInfo.userInfo
+                                    ? this.UserStore.authInfo.userInfo.photo
+                                    : 'https://files.authing.co/authing-console/default-user-avatar.png'
+                            }
+                        />
+                    }
+                    content={
+                        <p>
+                            We supply a series of design principles, practical patterns and high quality design
+                            resources (Sketch and Axure), to help people create their product prototypes beautifully
+                            and efficiently.
+                        </p>
+                    }
+                    datetime={
+                        <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
+                            <span>{moment().fromNow()}</span>
+                        </Tooltip>
+                    }
+                    children={this.props.children}
+                />
+                {this.showReplay && <CommentInput />}
+            </div>
         );
     }
 }
