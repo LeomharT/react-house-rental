7db06f809f706cef087f78d87ccec4f03a609399
LeomharT <leo.liao.lzy@gmail.com>
2021-09-29 08:07:46 +0800

    添加列表项

--- src/components/Community/CListItem.tsx
+++ src/components/Community/CListItem.tsx
@@ -1,13 +1,43 @@
-import React, { Component } from 'react';
-
-export default class CListItem extends Component
+import LikeOutlined from '@ant-design/icons/lib/icons/LikeOutlined';
+import MessageOutlined from '@ant-design/icons/lib/icons/MessageOutlined';
+import StarOutlined from '@ant-design/icons/lib/icons/StarOutlined';
+import { Avatar, List, Space } from 'antd';
+import { observer } from 'mobx-react';
+import React, { Component, FunctionComponent } from 'react';
+const IconText = (props: { icon: FunctionComponent, text: string; }) => (
+    <Space>
+        {React.createElement(props.icon)}
+        {props.text}
+    </Space>
+);
+@observer
+export default class CListItem extends Component<{ data: any; }, {}>
 {
     render()
     {
         return (
-            <div>
+            <List.Item
+                key={Symbol().toString()}
+                actions={[
+                    <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
+                    <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
+                    <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
+                ]}
+                extra={
+                    <img
+                        width={272}
+                        alt="logo"
+                        src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                    />
+                }>
 
-            </div>
+                <List.Item.Meta
+                    avatar={<Avatar src='https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png' />}
+                    title='ueserName'
+                    description='asdasdjoasi'
+                />
+                {this.props.data.content}
+            </List.Item>
         );
     }
 }
--- src/components/Community/Community.tsx
+++ src/components/Community/Community.tsx
@@ -1,14 +1,55 @@
 import { PlusOutlined } from '@ant-design/icons';
 import { Button, List, Radio } from 'antd';
+import { observable } from 'mobx';
 import { observer } from 'mobx-react';
 import React, { Component } from 'react';
 import SaySomething from '../../assets/img/SaySomething.svg';
 import '../../assets/scss/Community.scss';
 import HeadNavigate from '../Common/HeadNavigate';
+import CListItem from './CListItem';
 import CommunityCrowd from './CommunityCrowd';
+
 @observer
 export default class Community extends Component<{}, {}>
 {
+    @observable articles: any[] = [
+        {
+            href: 'https://ant.design',
+            title: `ant design part 1`,
+            avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+            description:
+                'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+            content:
+                'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+        },
+        {
+            href: 'https://ant.design',
+            title: `ant design part 1`,
+            avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+            description:
+                'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+            content:
+                'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+        },
+        {
+            href: 'https://ant.design',
+            title: `ant design part 1`,
+            avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+            description:
+                'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+            content:
+                'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+        },
+        {
+            href: 'https://ant.design',
+            title: `ant design part 1`,
+            avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+            description:
+                'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+            content:
+                'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+        }
+    ];
     componentDidMount()
     {
     }
@@ -40,17 +81,16 @@ export default class Community extends Component<{}, {}>
                     <div className='QuestionList'>
                         <List
                             itemLayout='vertical'
+                            dataSource={this.articles}
                             size='large'
                             renderItem={(item) =>
                             {
                                 return (
-                                    <List.Item>
-
-                                    </List.Item>
+                                    <CListItem data={item} />
                                 );
                             }}
-                        >
 
+                        >
                         </List>
                         <div className='CreatorEntrance'>
                             <img alt='saySomething' src={SaySomething} />
