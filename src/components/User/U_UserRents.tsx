95608594f17282b3f5059f6cb900a6a447756fe2
LeomharT <leo.liao.lzy@gmail.com>
2021-10-19 09:10:03 +0800

    修改为一张卡片

--- src/components/User/U_UserRents.tsx
+++ src/components/User/U_UserRents.tsx
@@ -1,15 +1,16 @@
-import { ExportOutlined, MoneyCollectOutlined, QrcodeOutlined, QuestionOutlined, ToolOutlined } from '@ant-design/icons';
-import { Badge, Button, Popover, Spin } from 'antd';
+import { EnvironmentOutlined, ExportOutlined, HomeOutlined, MoneyCollectOutlined, QrcodeOutlined, ToolOutlined } from '@ant-design/icons';
+import { Button, Card, Popover, Spin } from 'antd';
 import { observable } from 'mobx';
 import { observer } from 'mobx-react';
 import moment from 'moment';
-import React, { Component } from 'react';
+import React, { Component, createRef, RefObject } from 'react';
 import { HouseInfo } from '../../interfaces/HouseListInterface';
 import { UserRentListItem } from '../../interfaces/UserInferface';
 import HouseStore from '../../redux/HouseStore';
 import UserStore from '../../redux/UserStore';
-import HouseItem from '../HouseList/HouseItem';
+import { CONST_HOST } from '../Common/VariableGlobal';
 
+const { Meta } = Card;
 const RestOfRentDay = (checkOutDate: Date): number =>
 {
     return (
@@ -33,14 +34,49 @@ export default class U_UserRents extends Component<{}, {}>
 {
     UserStore: UserStore = UserStore.GetInstance();
     HouseStore: HouseStore = HouseStore.GetInstance();
+    tMap: RefObject<HTMLDivElement> = createRef<HTMLDivElement>();
     @observable userRentList: UserRentListItem[] = [];
     @observable houseInfo: HouseInfo;
+    InitTMap = (houseDetailInfo: HouseInfo): void =>
+    {
+        if (!houseDetailInfo?.baseInfo) return;
+        const map = new TMap.Map(this.tMap.current, {
+            center: new TMap.LatLng(
+                parseFloat(houseDetailInfo.detailInfo.hLatitude),
+                parseFloat(houseDetailInfo.detailInfo.hLongitude)),
+            zoom: 18,
+            pitch: 43.5,
+            rotation: 45,
+            viewMode: "2D"
+        });
+        new TMap.MultiMarker({
+            map: map,
+            style: {
+                markerStyle: new TMap.MarkerStyle({
+                    width: 25,
+                    height: 35,
+                    anchor: { x: 16, y: 32 }
+                })
+            },
+            geometries: [{
+                id: "1",
+                styled: "markerStyle",
+                position: new TMap.LatLng(
+                    parseFloat(houseDetailInfo.detailInfo.hLatitude),
+                    parseFloat(houseDetailInfo.detailInfo.hLongitude)),
+                properties: {
+                    title: "position01"
+                }
+            }]
+        });
+    };
     async componentDidMount()
     {
         this.userRentList = await this.UserStore.InitCurrentUserRentList(this.UserStore.GetCurrentUserId());
         if (this.userRentList.length)
         {
             this.houseInfo = await this.HouseStore.InitHouseInfo(this.userRentList[0].hId);
+            this.InitTMap(this.houseInfo);
         }
     }
     render()
@@ -52,54 +88,61 @@ export default class U_UserRents extends Component<{}, {}>
                 {userRentList.map((rentInfo: UserRentListItem) =>
                 {
                     return (
-                        <div className='RentItem' key={rentInfo.orderId}>
-                            <div className='RentTitle'>
-                                <Popover
-                                    content={<div>
-                                        您可以在这里选择退房和续租
+                        <Card
+                            hoverable
+                            key={rentInfo.orderId}
+                            cover={
+                                <div className='RentInfoCover'>
+                                    <img alt='cover' src={`${CONST_HOST}/${houseInfo.baseInfo.hExhibitImg}`} />
+                                    <div className='RentInfo'>
+                                        <div>{houseInfo.baseInfo.hTitle}</div>
+                                        <div>
+                                            <EnvironmentOutlined />{houseInfo.baseInfo.hRegion}
+                                            <HomeOutlined />{houseInfo.baseInfo.hLayout}
+                                        </div>
+                                        <div ref={this.tMap}>
+
+                                        </div>
                                     </div>
-                                    }>
-                                    <Badge
-                                        count={<QuestionOutlined />}
-                                        style={{
-                                            fontSize: "12px",
-                                            position: "absolute",
-                                            cursor: "pointer",
-                                        }}
-                                        children={
-                                            <div>我的租约({`${userRentList.length}`})---
-                                                <span style={{ color: RestOfRentDay(rentInfo.checkOutDate) > 10 ? '#52c41a' : "#fe615a" }}>
-                                                    剩余{RestOfRentDay(rentInfo.checkOutDate)}天
-                                                </span>
-                                            </div>
+                                </div>
+                            }
+                            actions={
+                                [
+                                    <Button size='large' children={'我要续租'} type='primary' icon={<MoneyCollectOutlined />} />,
+                                    <Button size='large' children={'我要报修'} type='primary' icon={<ToolOutlined />} />,
+                                    <Popover
+                                        trigger='click'
+                                        placement='top'
+                                        content={
+                                            <img alt='QRKey' src={RenderQrKey(rentInfo.checkOutDate)} />
                                         }
-                                    />
-                                </Popover>
-                            </div>
-                            <HouseItem HouseInfo={houseInfo.baseInfo} />
-                            <div className='RentInfo'>
-                                <span>
-                                    入住日期:{moment(rentInfo.checkInDate).format("YYYY-MM-DD")}
-                                </span>
-                                <span>
-                                    退房日期:{moment(rentInfo.checkOutDate).format("YYYY-MM-DD")}
-                                </span>
-                            </div>
-                            <div className='RentOptions'>
-                                <Button size='large' children={'我要续租'} type='primary' icon={<MoneyCollectOutlined />} />
-                                <Button size='large' children={'我要报修'} type='primary' icon={<ToolOutlined />} />
-                                <Popover
-                                    trigger='click'
-                                    placement='top'
-                                    content={
-                                        <img alt='QRKey' src={RenderQrKey(rentInfo.checkOutDate)} />
-                                    }
-                                >
-                                    <Button size='large' children={'我的开门码'} type='primary' icon={<QrcodeOutlined />} />
-                                </Popover>
-                                <Button size='large' children={'我要退租'} danger type='primary' icon={<ExportOutlined />} />
-                            </div>
-                        </div >
+                                    >
+                                        <Button size='large' children={'我的开门码'} type='primary' icon={<QrcodeOutlined />} />
+                                    </Popover>,
+                                    <Button size='large' children={'我要退租'} danger type='primary' icon={<ExportOutlined />} />,
+                                ]
+                            }
+                        >
+                            <Meta
+                                title={
+                                    <div className='RentRestOfDays' style={{
+                                        color: RestOfRentDay(rentInfo.checkOutDate) > 10 ? "#52c41a" : '#fe615a'
+                                    }}>
+                                        剩余{RestOfRentDay(rentInfo.checkOutDate)}天
+                                    </div>
+                                }
+                                description={
+                                    <div className='CheckDate'>
+                                        <span>
+                                            入住日期:{moment(rentInfo.checkInDate).format("YYYY-MM-DD")}
+                                        </span>
+                                        <span>
+                                            退房时间:{moment(rentInfo.checkOutDate).format("YYYY-MM-DD")}
+                                        </span>
+                                    </div>
+                                }
+                            />
+                        </Card>
                     );
                 })
                 }
