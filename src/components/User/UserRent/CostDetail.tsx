4723f4248fa641f2cd4195546c3a5e7642e0d937
LeomharT <leo.liao.lzy@gmail.com>
2021-10-22 16:09:35 +0800

    修正bug，添加水电费缴费按钮，但是不做就是玩

--- src/components/User/UserRent/CostDetail.tsx
+++ src/components/User/UserRent/CostDetail.tsx
@@ -6,14 +6,14 @@ import { IconFont } from '../../HouseList/RentAndPay/ConfirmOrder';
 
 export const FormatNum = (str: string) =>
 {
-    var newStr = "";
-    var count = 0;
+    let newStr = "";
+    let count = 0;
     // 当数字是整数
-    if (str.indexOf(".") == -1)
+    if (str.indexOf(".") === -1)
     {
-        for (var i = str.length - 1; i >= 0; i--)
+        for (let i = str.length - 1; i >= 0; i--)
         {
-            if (count % 3 == 0 && count != 0)
+            if (count % 3 === 0 && count !== 0)
             {
                 newStr = str.charAt(i) + "," + newStr;
             } else
@@ -28,9 +28,9 @@ export const FormatNum = (str: string) =>
     // 当数字带有小数
     else
     {
-        for (var i = str.indexOf(".") - 1; i >= 0; i--)
+        for (let i = str.indexOf(".") - 1; i >= 0; i--)
         {
-            if (count % 3 == 0 && count != 0)
+            if (count % 3 === 0 && count !== 0)
             {
                 newStr = str.charAt(i) + "," + newStr;
             } else
@@ -54,28 +54,28 @@ export default class CostDetail extends Component<{ rentInfo: UserRentListItem; 
                     <Button size='large' icon={<PayCircleOutlined />} type='primary' />
                     <span>房屋金额</span>
                     <Progress percent={100} size='default' />
-                    <span>&yen;{FormatNum(parseFloat(rentInfo.totalAmount).toFixed(2))}</span>
+                    <div>&yen;{FormatNum(parseFloat(rentInfo.totalAmount).toFixed(2))}</div>
                 </div>
                 <Divider />
                 <div className='CostItem'>
                     <Button size='large' icon={<IconFont type='icon-houserentshuifei' />} type='primary' />
                     <span>水费</span>
                     <Progress percent={60} size='default' />
-                    <span>&yen;60/&yen;100</span>
+                    <div>&yen;60/&yen;100<Button children='缴费' type='link' /></div>
                 </div>
                 <Divider />
                 <div className='CostItem'>
                     <Button size='large' icon={<ThunderboltOutlined />} type='primary' />
                     <span>电费</span>
                     <Progress percent={40} size='default' />
-                    <span>&yen;40/&yen;100</span>
+                    <div>&yen;40/&yen;100<Button children='缴费' type='link' /></div>
                 </div>
                 <Divider />
                 <div className='CostItem'>
                     <Button size='large' icon={<IconFont type='icon-houserenttingchejiaofei' />} type='primary' />
                     <span>停车费</span>
                     <Progress percent={70} size='default' />
-                    <span>&yen;70/&yen;100</span>
+                    <div>&yen;70/&yen;100<Button children='缴费' type='link' /></div>
                 </div>
             </div>
         );
