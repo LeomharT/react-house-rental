58d5ee91b5cf3ff7b2a264bd26d20f9d14318124
LeomharT <leo.liao.lzy@gmail.com>
2021-11-07 10:55:43 +0800

    UI

--- src/components/User/U_UserCollect.tsx
+++ src/components/User/U_UserCollect.tsx
@@ -1,12 +1,15 @@
+import { LeftOutlined } from '@ant-design/icons';
 import DeleteOutlined from '@ant-design/icons/lib/icons/DeleteOutlined';
 import ExclamationOutlined from '@ant-design/icons/lib/icons/ExclamationOutlined';
 import { Button, Empty, Popconfirm } from 'antd';
 import { observable } from 'mobx';
 import { observer } from 'mobx-react';
 import React, { Component, createRef, RefObject } from 'react';
+import '../../assets/scss/WishList.scss';
 import { HouseBaseInfo } from '../../interfaces/HouseListInterface';
 import HouseStore from '../../redux/HouseStore';
 import UserStore from '../../redux/UserStore';
+import HeadNavigate from '../Common/HeadNavigate';
 import { CONST_HOST } from '../Common/VariableGlobal';
 import HouseItem from '../HouseList/HouseItem';
 
@@ -24,8 +27,9 @@ export default class U_UserCollect extends Component
     };
     InitMap = () =>
     {
-        const map = new TMap.Map(this.tMapRef.current, {
-            zoom: 18,
+        new TMap.Map(this.tMapRef.current, {
+            center: new TMap.LatLng(26.100271, 119.295624),
+            zoom: 11,
             pitch: 43.5,
             rotation: 45,
             viewMode: "2D"
@@ -33,54 +37,65 @@ export default class U_UserCollect extends Component
     };
     async componentDidMount()
     {
+        if (!this.UserStore.CheckForIsLogin()) return;
         await this.InitUserCollections();
         this.InitMap();
     }
     render()
     {
         const { userCollections, UserStore, HouseStore } = this;
-        if (!this.UserStore.CheckForIsLogin()) return null;
-        if (!userCollections.length) return (<Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description='暂时没有收藏任何房源' />);
         return (
             <div className='U_Collections'>
-                {
-                    userCollections.map((h: HouseBaseInfo) =>
-                    {
-                        return (
-                            <div className='U_CollectWrapper' key={h.hId}>
-                                <HouseItem HouseInfo={h} />
-                                <Popconfirm
-                                    title='确定要删除吗?'
-                                    placement='bottom'
-                                    okType='danger'
-                                    okText='确定'
-                                    cancelText='取消'
-                                    icon={
-                                        <ExclamationOutlined style={{ color: 'red' }} />
-                                    }
-                                    onConfirm={() =>
-                                    {
-                                        HouseStore.DeleteCurrentHouseFromUserCollections(
-                                            UserStore.authInfo.userInfo.id,
-                                            h.hId,
-                                            this.InitUserCollections
-                                        );
-                                    }}
-                                >
-                                    <Button
-                                        type='text'
-                                        className='DeleteCollectBtn'
-                                        danger
-                                        size='large'
-                                        icon={
-                                            <DeleteOutlined />
-                                        } />
-                                </Popconfirm>
-                            </div>
-                        );
-                    })
+                <HeadNavigate />
+                {(!userCollections.length)
+                    ? (<Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description='暂时没有收藏任何房源' />)
+                    : <div className='WishList'>
+                        <div className='ListContent'>
+                            <h1>
+                                <Button icon={<LeftOutlined />} type='link' />
+                                心愿单
+                            </h1>
+                            {userCollections.map((h: HouseBaseInfo) =>
+                            {
+                                return (
+                                    <div className='U_CollectWrapper' key={h.hId}>
+                                        <HouseItem HouseInfo={h} />
+                                        <Popconfirm
+                                            title='确定要删除吗?'
+                                            placement='bottom'
+                                            okType='danger'
+                                            okText='确定'
+                                            cancelText='取消'
+                                            icon={
+                                                <ExclamationOutlined style={{ color: 'red' }} />
+                                            }
+                                            onConfirm={() =>
+                                            {
+                                                HouseStore.DeleteCurrentHouseFromUserCollections(
+                                                    UserStore.authInfo.userInfo.id,
+                                                    h.hId,
+                                                    this.InitUserCollections
+                                                );
+                                            }}
+                                        >
+                                            <Button
+                                                type='text'
+                                                className='DeleteCollectBtn'
+                                                danger
+                                                size='large'
+                                                icon={
+                                                    <DeleteOutlined />
+                                                } />
+                                        </Popconfirm>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                        <div
+                            ref={this.tMapRef}
+                            className='MapArea' />
+                    </div>
                 }
-                <div ref={this.tMapRef} style={{ width: "50%" }} />
             </div>
         );
     }
