09f8c2312e69dfa9510ee66c5bf7aa7b5147bf89
LeomharT <leo.liao.lzy@gmail.com>
2021-11-12 10:09:03 +0800

    切换报表图形

--- src/components/User/UserRent/CostDetail.tsx
+++ src/components/User/UserRent/CostDetail.tsx
@@ -1,5 +1,5 @@
 import { PayCircleOutlined, ThunderboltOutlined } from '@ant-design/icons';
-import { Button, Divider, Progress } from 'antd';
+import { Button, Divider, Progress, Switch } from 'antd';
 import * as echarts from 'echarts';
 import React, { Component, createRef, RefObject } from 'react';
 import { UserRentListItem } from '../../../interfaces/UserInferface';
@@ -44,50 +44,79 @@ export const FormatNum = (str: string): string =>
         return str;
     }
 };
+enum EcharState
+{
+    bar = 'bar',
+    pie = 'pie',
+}
 
 export default class CostDetail extends Component<{ rentInfo: UserRentListItem; }, {}>
 {
     echartsEl: RefObject<HTMLDivElement> = createRef<HTMLDivElement>();
-    InitEchart = (): void =>
+    echart: echarts.ECharts | undefined = undefined;
+    InitEchart = (state: EcharState): void =>
     {
-        const echart = echarts.init(this.echartsEl.current as HTMLElement);
+        if (!this.echart)
+            this.echart = echarts.init(this.echartsEl.current as HTMLElement);
         const option: echarts.EChartsOption = {
             title: {
-                text: '费用详情'
+                text: "费用详情"
             },
             tooltip: {
                 show: true,
                 trigger: 'item',
-            },
-            series: [
-                {
-                    type: 'pie',
-                    data: [
-                        {
-                            value: parseInt(this.props.rentInfo.totalAmount),
-                            name: "房租",
-                        },
-                        {
-                            value: 200,
-                            name: '水费'
-                        },
-                        {
-                            value: 300,
-                            name: '电费'
-                        },
-                        {
-                            value: 400,
-                            name: '停车费'
-                        },
-                    ],
-                }
-            ],
+            }
         };
-        echart.setOption(option);
+        switch (state)
+        {
+            case EcharState.bar:
+                Object.assign(option, {
+                    xAxis: {
+                        data: ['房租', '水费', '电费', '停车费']
+                    },
+                    yAxis: {},
+                    series: {
+                        type: "bar",
+                        data: [parseInt(this.props.rentInfo.totalAmount), 200, 300, 400]
+                    }
+                } as echarts.EChartsOption);
+                break;
+            case EcharState.pie:
+                Object.assign(option, {
+                    series: [
+                        {
+                            type: 'pie',
+                            data: [
+                                {
+                                    value: parseInt(this.props.rentInfo.totalAmount),
+                                    name: "房租",
+                                },
+                                {
+                                    value: 200,
+                                    name: '水费'
+                                },
+                                {
+                                    value: 300,
+                                    name: '电费'
+                                },
+                                {
+                                    value: 400,
+                                    name: '停车费'
+                                },
+                            ],
+                        }
+                    ],
+                } as echarts.EChartsOption);
+                break;
+            default:
+                break;
+        }
+        this.echart.clear();
+        this.echart.setOption(option);
     };
     componentDidMount()
     {
-        this.InitEchart();
+        this.InitEchart(EcharState.pie);
     }
     render()
     {
@@ -122,8 +151,16 @@ export default class CostDetail extends Component<{ rentInfo: UserRentListItem; 
                     <div>&yen;70/&yen;100<Button children='缴费' type='link' /></div>
                 </div>
                 <div className='EchartsArea' ref={this.echartsEl}>
-
                 </div>
+                <Switch
+                    style={{ width: "30px" }} checkedChildren='柱' unCheckedChildren='饼'
+                    onChange={(e) =>
+                    {
+                        if (e)
+                            this.InitEchart(EcharState.bar);
+                        else
+                            this.InitEchart(EcharState.pie);
+                    }} />
             </div>
         );
     }
