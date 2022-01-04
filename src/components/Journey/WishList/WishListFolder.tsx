7e3dd27f4bffa21882973790d14ba229ae53850b
LeomharT <leo.liao.lzy@gmail.com>
2022-01-04 17:13:09 +0800

    布局

--- src/components/Journey/WishList/WishListFolder.tsx
+++ src/components/Journey/WishList/WishListFolder.tsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { RouteComponentProps } from 'react-router-dom';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
 import HeadNavigate from '../../Common/HeadNavigate';
 
-export default class WishListFolder extends Component<RouteComponentProps, {}>
+class WishListFolder extends Component<RouteComponentProps, {}>
 {
     render()
     {
@@ -10,9 +10,34 @@ export default class WishListFolder extends Component<RouteComponentProps, {}>
             <div className='WishListFolder'>
                 <HeadNavigate />
                 <div className='FolderList'>
-
+                    <FolderItem />
                 </div>
             </div>
         );
     }
 }
+
+
+class FolderItem extends Component<{}, {}>
+{
+    render(): React.ReactNode
+    {
+        return (
+            <div className='FolderItem'>
+                <div className='FolderLeft'>
+
+                </div>
+                <div className='FolderRight'>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
+};
+
+export default withRouter(WishListFolder);
