8b5c051d626e369ff36af7974a4afdb2e5d7f2a2
LeomharT <leo.liao.lzy@gmail.com>
2022-01-05 11:23:29 +0800

    布局

--- src/components/Journey/WishList/WishListFolder.tsx
+++ src/components/Journey/WishList/WishListFolder.tsx
@@ -1,40 +1,79 @@
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { Card } from 'antd';
+import moment from 'moment';
 import React, { Component } from 'react';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
+import FolderEmpty from '../../../assets/img/Folder-Empty.gif';
+import UserStore from '../../../redux/UserStore';
 import HeadNavigate from '../../Common/HeadNavigate';
+
 class WishListFolder extends Component<RouteComponentProps, {}>
 {
+    UserStore: UserStore = UserStore.GetInstance();
+    componentDidMount(): void
+    {
+        const uID = this.UserStore.GetCurrentUserId();
+    }
     render()
     {
         return (
             <div className='WishListFolder'>
                 <HeadNavigate />
                 <div className='FolderList'>
-                    <FolderItem />
+                    <FolderItem folderID='0' folderName='默认合集' />
                 </div>
             </div>
         );
     }
 }
 
-
-class FolderItem extends Component<{}, {}>
+interface FolderItemProps
 {
+    FolderExhibit?: any[];
+    folderID: string,
+    folderName: string,
+}
+class FolderItem extends Component<FolderItemProps, {}>
+{
+    UserStore: UserStore = UserStore.GetInstance();
     render(): React.ReactNode
     {
+        console.log(this.UserStore.authInfo);
         return (
-            <div className='FolderItem'>
-                <div className='FolderLeft'>
-
-                </div>
-                <div className='FolderRight'>
-                    <div>
+            <Card className='FolderItem'
+                actions={
+                    [
+                        <SettingOutlined key="setting" />,
+                        <EditOutlined key="edit" />,
+                        <EllipsisOutlined key="ellipsis" />,
+                    ]
+                }
+                cover={
+                    <div className='FolderCover'>
+                        {
+                            this.props.FolderExhibit
+                                ? <>
+                                    <div className='FolderLeft'>
+                                        <img alt='Empty' src={FolderEmpty} />
+                                    </div>
+                                    <div className='FolderRight'>
+                                        <div>
+                                            <img alt='Empty' src={FolderEmpty} />
+                                        </div>
+                                        <div>
+                                            <img alt='Empty' src={FolderEmpty} />
+                                        </div>
+                                    </div>
+                                </>
+                                : <img alt='Empty' src={FolderEmpty} />
+                        }
 
                     </div>
-                    <div>
-
-                    </div>
-                </div>
-            </div>
+                }
+            >
+                <Card.Meta title='默认文件夹'
+                    description={"创建于:" + moment(this.UserStore.authInfo?.userInfo?.createdAt as Date).format('YYYY年MM月DD日 hh时mm分ss秒')} />
+            </Card>
         );
     }
 };
