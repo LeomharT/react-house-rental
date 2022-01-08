4f8e982d7f66b21404ab62ff8e18d943b91bcc36
LeomharT <leo.liao.lzy@gmail.com>
2022-01-08 10:42:10 +0800

    和转大写差不多哦，都是从左边开始处理，没隔3位加逗号即可

--- src/components/User/UserRent/CostDetail.tsx
+++ src/components/User/UserRent/CostDetail.tsx
@@ -5,43 +5,28 @@ import React, { Component, createRef, RefObject } from 'react';
 import { UserRentListItem } from '../../../interfaces/UserInferface';
 import { IconFont } from '../../HouseList/RentAndPay/ConfirmOrder';
 
-export const FormatNum = (str: string): string =>
+export const FormatNum = (num: string): string =>
 {
-    let newStr = "";
-    let count = 0;
-    // 当数字是整数
-    if (str.indexOf(".") === -1)
+    num = String(Number(num).toFixed(2));
+
+    const numbers = num.split(".");
+
+    let value = numbers[0].split('').reverse().reduce((prve, curr, index) =>
     {
-        for (let i = str.length - 1; i >= 0; i--)
+        let str = prve;
+        if (index % 3 === 0 && index !== 0)
         {
-            if (count % 3 === 0 && count !== 0)
-            {
-                newStr = str.charAt(i) + "," + newStr;
-            } else
-            {
-                newStr = str.charAt(i) + newStr;
-            }
-            count++;
+            str += ",";
         }
-        str = newStr + ".00"; //自动补小数点后两位
-        return str;
-    }
-    // 当数字带有小数
-    else
+        return str + curr;
+    }, '').split('').reverse().join('');
+
+    if (num.includes(".") && Number(numbers[1]) !== 0)
     {
-        for (let i = str.indexOf(".") - 1; i >= 0; i--)
-        {
-            if (count % 3 === 0 && count !== 0)
-            {
-                newStr = str.charAt(i) + "," + newStr;
-            } else
-            {
-                newStr = str.charAt(i) + newStr; //逐个字符相接起来
-            }
-            count++;
-        }
-        str = newStr + (str + "00").substr((str + "00").indexOf("."), 3);
-        return str;
+        return value + `.${numbers[1]}`;
+    } else
+    {
+        return value + '.00';
     }
 };
 enum EcharState
